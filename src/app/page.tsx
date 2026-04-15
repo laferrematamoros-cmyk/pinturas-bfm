f0857ab90351fed8fe36ff5e647ecf05916e6c36
laferrematamoros-cmyk <laferrematamoros@gmail.com>
2026-04-15 16:12:52 -0500

    Cambiar título a 'Elige tu color favorito'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1111,7 +1111,7 @@ export default function Home() {
         <>
             {/* Title */}
             <h1 className="text-center text-3xl font-light text-gray-800 mb-5">
-              Elige tu paleta de colores
+              Elige tu color favorito
             </h1>
 
             {/* Search */}
